5910840fa6083a63e26a18be1487dd955a23cb08
MN - TG <mntg3509@gmail.com>
2025-03-16 09:20:00 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -14,19 +14,3 @@ async def start(client: Private_Bots, msg: Message):
         reply_markup=INLINE.START_BTN,
     )
 
-
-@Private_Bots.on_message(
-    (filters.channel & filters.forwarded)
-    | (filters.private & filters.incoming)
-    | (filters.group & filters.forwarded)
-)
-async def remove(client: Private_Bots, msg: Message):
-    try:
-        (
-            await msg.copy(msg.chat.id, caption=msg.caption or None)
-            if msg.media and not msg.video_note and not msg.sticker
-            else await msg.copy(msg.chat.id)
-        )
-        await msg.delete()
-    except FloodWait as e:
-        await asyncio.sleep(e.value)
