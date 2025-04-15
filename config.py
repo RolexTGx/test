0955126a07888bf919a989feafb329477ffc8209
MN-TG <mohammedmusammiln@gmail.com>
2025-04-15 15:46:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,6 @@ class BOT:
     """
     TOKEN: Bot token generated from @BotFather
     """
-
     TOKEN = os.environ.get("TOKEN", "")
 
 
@@ -18,7 +17,6 @@ class API:
     HASH: Telegram API hash from https://my.telegram.org
     ID = Telegram API ID from https://my.telegram.org
     """
-
     HASH = os.environ.get("API_HASH", "")
     ID = int(os.environ.get("API_ID", 0))
 
@@ -27,20 +25,25 @@ class OWNER:
     """
     ID: Owner's user id, get it from @userinfobot
     """
-
     ID = int(os.environ.get("OWNER", 0))
+
 
 class CHANNEL:
     """
     ID: Telegram Channel ID where the bot will post automatically
     """
-
     ID = int(os.environ.get("CHANNEL_ID", 0))
+
+
+class RARE_CHANNEL:
+    """
+    ID: Rare Telegram Channel ID for special torrent posts
+    """
+    ID = int(os.environ.get("RARE_CHANNEL_ID", 0))
 
 
 class WEB:
     """
     PORT: Specific port no. on which you want to run your bot, DON'T TOUCH IT IF YOU DON'T KNOW WHAT IS IT.
     """
-
     PORT = int(os.environ.get("PORT", 8000))
